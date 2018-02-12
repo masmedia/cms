199459357f6c2febfd3045cad5d4155d66141da6
masmedia <mustafa.sari.media@gmail.com>
2018-02-13 00:13:45 +0300

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  
+  # Root route
+  root 'demo#index'
+
+  get 'demo/index'  # Shorter form of "Match route". Longer one is below
+  # match "demo/index", :to => "demo#index", :via => :get
+
+  # get ':controller(/:action(/:id))'  # Default route. Longer version is below
+  # match ":controller(/:action(/:id))", :via => :get
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
