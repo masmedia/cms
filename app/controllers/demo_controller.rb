8a4c0567f3b52975f7ee8be4d9a59fa1b319cdd1
masmedia <mustafa.sari.media@gmail.com>
2018-02-13 00:53:53 +0300

    Added render into actions to render a view template

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,10 +3,15 @@ class DemoController < ApplicationController
   layout false
   
   def index
-
+  	render('index')
+  	# These are also possione
+  	# render(:template => 'demo/index')
+  	# render('demo/index')
   end
 
+  # This is an action 
   def hello
+  	render('hello')  # This will render a template
 
   end
 
