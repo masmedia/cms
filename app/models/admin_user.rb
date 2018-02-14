e563c7dbf8c2c58362b4fa1a023a98eaedf506cc
masmedia <mustafa.sari.media@gmail.com>
2018-02-14 13:37:51 +0300

    Sync the admin_users table with the Rails class to comply with convention

---
+++ app/models/admin_user.rb
@@ -0,0 +1,9 @@
+class AdminUser < ApplicationRecord
+# class User < ApplicationRecord  # Old table name
+	# If you have changed the db name or inherit a legacy db code from some codebase, 
+	# either add the line self.table_name = "legacy_db_table_name" or change the 
+	# file name to the lower_case_singular - separated file name and then change
+	# class name to CamelCase db_table_name to syncronize db with Rails conventions.
+
+	# self.table_name = "admin_users"
+end
