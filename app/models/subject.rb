4ba5dbf2f581040f2b8779668c6225fb3e933beb
masmedia <mustafa.sari.media@gmail.com>
2018-02-18 18:08:28 +0300

    Added named scopes for displaying db records by visibility, sorting them, and searching in them

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,14 @@
 class Subject < ApplicationRecord
+
+
+	# Named scopes are evaluated when called!
+	scope :visible, lambda { where(:visible => true)   }
+	scope :invisible, lambda { where(:visible => false)   }
+	scope :sorted, lambda { order("position ASC")   }
+	scope :newest_first, lambda { order("created_at DESC") }
+
+	# The query might come from user. % sign interpolates the string coming from 
+	# the user and ? mark sanitizes it and makes it safe from SQL injection
+	scope :search, lambda {  |query|  where(["name LIKE ?", "%#{query}%"])  }
+
 end
