b21f9cd27afa54d388d862cdaa7ccbaf586b4e24
masmedia <mustafa.sari.media@gmail.com>
2018-02-19 23:44:36 +0300

    Updated 1:1 relationship between 'Subject' and 'Page' to one-to-many

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,8 +1,8 @@
 class Subject < ApplicationRecord
 
-	# Added one-to-one relationship. The subject 'has_one' page
+	# Added one-to-many relationship. The subject 'has_many' pages
 	# Don't forget to add page belongs_to :subject in Page class
-	has_one :page
+	has_many :pages
 
 	# Named scopes are evaluated when called!
 	scope :visible, lambda { where(:visible => true)   }
