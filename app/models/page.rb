d0fb54a870527914000706461583ca334888da34
masmedia <mustafa.sari.media@gmail.com>
2018-02-20 00:22:19 +0300

    Added 1:M relationship between 'Page' and 'Section'

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,6 @@ class Page < ApplicationRecord
 	# Don't forget to add subject 'has_one' page in Subject class
 	# Also check the db migrations that a foreign key was added in pages table
 	belongs_to :subject
+	has_many :sections
 
 end
