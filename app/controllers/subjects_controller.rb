2787713b9a1eb38caf42271f16d54c74da7cbda0
masmedia <mustafa.sari.media@gmail.com>
2018-02-26 14:52:25 +0300

    Added actions 'create', 'update', and 'destroy' for CRUD operations

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,16 +1,29 @@
 class SubjectsController < ApplicationController
+  # Actions related to reading
   def index
   end
 
   def show
   end
 
+  # Actions related to creation
   def new
   end
 
+  def create
+  end
+
+  # Actions related to updating
   def edit
   end
 
+  def update
+  end
+
+  # Actions related to deletion
   def delete
+  end
+
+  def destroy
   end
 end
