66a24e51cdd1dfc466f599801fd8eb9ad3f936d1
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 01:54:25 +0300

    Implemented 'new' and 'create' actions and view template to 'Pages' controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,18 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
   end
 
+  # If subject_id does not exist in db, it won't add to the db
+  # since subject_id is a foreign key for subjects table
   def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -23,6 +32,11 @@ class PagesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def page_params
+    params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
   end
 
 end
