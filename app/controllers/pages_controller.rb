cd7cdd46e030832e3c94341ab6989b184a99b692
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 01:54:25 +0300

    Implemented 'edit' and 'update' actions and view template to 'Pages' controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,21 +8,42 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
   end
 
+  # If subject_id does not exist in db, it won't add to the db
+  # since subject_id is a foreign key for subjects table
   def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      redirect_to(page_path(@page) )
+    else
+      render('edit')
+    end
   end
 
   def delete
   end
 
   def destroy
+  end
+
+  private
+  def page_params
+    params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
   end
 
 end
