703c23a0d32ea241c271da3a6d2c7a94fb27cd9a
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 01:46:25 +0300

    Implemented 'show' action and view template to 'Pages' controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,25 @@ class PagesController < ApplicationController
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 
   def delete
   end
+
+  def destroy
+  end
+
 end
