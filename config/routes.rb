1b5ec1fc55e6e69458525e9413879fe936a6759c
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 01:02:10 +0300

    Removed match routes for 'Pages' and 'Sections' controllers and added resourceful routes instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,22 @@
 Rails.application.routes.draw do
-  
-  get 'sections/index'
-
-  get 'sections/show'
-
-  get 'sections/new'
-
-  get 'sections/edit'
-
-  get 'sections/delete'
-
-  get 'pages/index'
-
-  get 'pages/show'
-
-  get 'pages/new'
-
-  get 'pages/edit'
-
-  get 'pages/delete'
-
-  # get 'subjects/index'
-  # get 'subjects/show'
-  # get 'subjects/new'
-  # get 'subjects/edit'
-  # get 'subjects/delete'
 
   # Root route
   root 'demo#index'
 
   # Resourceful routes
   resources :subjects do
+    member do
+      get :delete   # Add eighth CRUD action which is :delete
+    end    
+  end
+
+  resources :pages do
+    member do
+      get :delete   # Add eighth CRUD action which is :delete
+    end    
+  end
+
+  resources :sections do
     member do
       get :delete   # Add eighth CRUD action which is :delete
     end    
