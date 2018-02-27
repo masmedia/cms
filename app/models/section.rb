9634e5626913141ff358779d7859c2ec93f41f16
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 01:10:05 +0300

    Added named scopes to 'Page' and 'Subject' model

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,4 +5,11 @@ class Section < ApplicationRecord
 	has_many :section_edits
 	has_many :admin_users, :through => :section_edits
 
+	# Named scopes
+	scope :visible, lambda { where(:visible => true)   }
+	scope :invisible, lambda { where(:visible => false)   }
+	scope :sorted, lambda { order("position ASC")   }
+	scope :newest_first, lambda { order("created_at DESC") }
+	scope :search, lambda {  |query|  where(["name LIKE ?", "%#{query}%"])  }
+
 end
