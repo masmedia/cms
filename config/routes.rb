65adcaa310b4c883c998a427a0ed59ec78ccfd6e
masmedia <mustafa.sari.media@gmail.com>
2018-02-28 00:57:45 +0300

    Added 'Sections' controller with index, show, new, edit, delete actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'sections/index'
+
+  get 'sections/show'
+
+  get 'sections/new'
+
+  get 'sections/edit'
+
+  get 'sections/delete'
+
   get 'pages/index'
 
   get 'pages/show'
