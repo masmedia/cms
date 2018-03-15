6e197d0c462ab2ac1e6d901775819a9cb51c7860
masmedia <mustafa.sari.media@gmail.com>
2018-03-15 18:29:44 +0300

    Added layout to admin area and filled login and menu forms

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,20 @@
 class AccessController < ApplicationController
+  
+	layout 'admin'
+
   def menu
+  	# display text and links
   end
 
   def login
+  	# login form
   end
+
+  def attempt_login
+
+  end
+
+  def logout
+  end
+
 end
